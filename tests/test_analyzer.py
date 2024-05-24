b5fb05db540a1bb5677f2a3886034b38911bee80
Esra Perdomo <93940994+eperdomo97@users.noreply.github.com>
2024-05-24 15:16:02 -0700

    Update test_analyzer.py
    fixed error by getting short file path names using ctype library

--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -2,6 +2,25 @@ import tkinter as tk
 from tkinter import filedialog
 import os
 from os.path import join, getsize
+import ctypes
+from ctypes import wintypes
+_GetShortPathNameW = ctypes.windll.kernel32.GetShortPathNameW
+_GetShortPathNameW.argtypes = [wintypes.LPCWSTR, wintypes.LPWSTR, wintypes.DWORD]
+_GetShortPathNameW.restype = wintypes.DWORD
+
+
+def get_short_path_name(long_name):
+    #Gets the short path name of a given long path.
+    #http://stackoverflow.com/a/23598461/200291
+    output_buf_size = 0
+    while True:
+        output_buf = ctypes.create_unicode_buffer(output_buf_size)
+        needed = _GetShortPathNameW(long_name, output_buf, output_buf_size)
+        if output_buf_size >= needed:
+            return output_buf.value
+        else:
+            output_buf_size = needed
+
 
 def analyze_directory():
     #tkinter dialog box for directory selection
@@ -15,6 +34,7 @@ def analyze_directory():
     totalBytes = 0
 
     for root, dirs, files in os.walk(directory, topdown=False):
+        root = get_short_path_name(root)
         dirBytes = sum(getsize(join(root, name)) for name in files)
         dirsizes[root] = dirBytes
         totalBytes += dirBytes
@@ -26,10 +46,10 @@ def analyze_directory():
     print("File sizes:", filesizes)
 
 def main():
-    root = tk.Tk()
-    root.withdraw()
+    # root = tk.Tk()
+    # root.withdraw()
     analyze_directory()
-    root.mainloop()
+    # root.mainloop()
 
 if __name__ == "__main__":
     main()
